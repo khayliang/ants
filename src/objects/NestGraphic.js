6dae795abb789acf6c9a001335e89e2e5e138d3d
khayliang <khayliang@gmail.com>
2021-06-04 21:22:23 +0800

    change color of nest

--- src/objects/NestGraphic.js
+++ src/objects/NestGraphic.js
@@ -4,7 +4,7 @@ import '@pixi/graphics-extras'
 export default class extends Graphics {
   constructor() {
     super()
-    this.beginFill(0xffffff)
+    this.beginFill(0x228B22)
     this.drawCircle(0, 0, 20)
     this.endFill()
     this.zIndex = 1
