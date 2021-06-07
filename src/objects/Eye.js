c2c1d2867f454f30543d856695165e2a31e76fe8
khayliang <khayliang@gmail.com>
2021-06-07 23:37:07 +0800

    add fn for eye to get class instance

--- src/objects/Eye.js
+++ src/objects/Eye.js
@@ -1,15 +1,20 @@
 /* eslint class-methods-use-this: ["error", { "exceptMethods": ["getObjectsAtCoords"] }] */
 
 export default class Eye {
-  constructor({ object, getObjectsAtCoords, viewDistance = 10, fov = 0.52 }) {
+  constructor({ object, getObjectsAtCoords, getClassInstancesAtCoords, viewDistance = 10, fov = 0.52 }) {
     this.object = object
     this.viewDistance = viewDistance
     this.fov = fov
 
     this.getObjectsAtCoords = getObjectsAtCoords
+    this.getClassInstancesAtCoords = getClassInstancesAtCoords
   }
 
   getObjectsAtCoords() {
+    return []
+  }
+
+  getClassInstancesAtCoords() {
     return []
   }
 
@@ -31,5 +36,25 @@ export default class Eye {
         y: currCoords.y + this.viewDistance * Math.sin(currRad - this.fov),
       },
     ])
+  }
+
+  getNearbyClassInstances(classType) {
+    const currCoords = this.object.getCoords()
+    const currRad = this.object.getRadians()
+    return this.getClassInstancesAtCoords([
+      currCoords,
+      {
+        x: currCoords.x + this.viewDistance * Math.cos(currRad),
+        y: currCoords.y + this.viewDistance * Math.sin(currRad),
+      },
+      {
+        x: currCoords.x + this.viewDistance * Math.cos(currRad + this.fov),
+        y: currCoords.y + this.viewDistance * Math.sin(currRad + this.fov),
+      },
+      {
+        x: currCoords.x + this.viewDistance * Math.cos(currRad - this.fov),
+        y: currCoords.y + this.viewDistance * Math.sin(currRad - this.fov),
+      },
+    ], classType)
   }
 }
