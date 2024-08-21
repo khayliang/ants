a6eb88099383f4a38bfa2286710c2aebef957793
khayliang <khayliang@gmail.com>
2024-08-22 00:08:19 +0800

    test: add test for following navigation pheromone

--- src/objects/Ant/StateHeldFood.test.js
+++ src/objects/Ant/StateHeldFood.test.js
@@ -1,6 +1,7 @@
 import Food from '../Food'
 import Nest from '../Nest'
 import PheromoneFood from '../PheromoneFood'
+import PheromoneNavigation from '../PheromoneNavigation'
 import Ant from './Ant'
 import StateHeldFood from './StateHeldFood'
 
@@ -33,4 +34,50 @@ test('When eye detects nest, ant changes direction to nest', () => {
   const newDirection = state.getDirection()
   expect(typeof newDirection).toEqual('number')
   expect(newDirection).not.toEqual(initialRadians)
+})
+
+test('Ant moves right if navigation pheromone right', () => {
+  const ant = new Ant({
+    speed: 1,
+    initialRadians: 0
+  })
+  const pheromone = new PheromoneNavigation()
+
+  ant.setCoords({ x: 0, y: 0 })
+  pheromone.setCoords({ x: 5, y: 5 })
+
+  ant.setEye({
+    getNearbyObjects: () => [pheromone],
+    getNearbyClassInstances: (instance) => {
+      if (instance === PheromoneNavigation) return [pheromone]
+      else return []
+    },
+  })
+  const state = new StateHeldFood(ant)
+
+  const direction = state.getDirection()
+
+  expect(direction > 0).toEqual(true)
+})
+
+test('Ant moves left if navigation pheromone left', () => {
+  const ant = new Ant({
+    speed: 1,
+    initialRadians: 0
+  })
+  const pheromone = new PheromoneNavigation()
+
+  ant.setCoords({ x: 0, y: 5 })
+  pheromone.setCoords({ x: 5, y: 0 })
+  ant.setEye({
+    getNearbyObjects: () => [pheromone],
+    getNearbyClassInstances: (instance) => {
+      if (instance === PheromoneNavigation) return [pheromone]
+      else return []
+    },
+  })
+  const state = new StateHeldFood(ant)
+
+  const direction = state.getDirection()
+  expect(direction < 0).toEqual(true)
 })
